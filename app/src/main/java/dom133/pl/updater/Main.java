1bc16e82e597c6fae86de418cee46c9674316302
dom133 <dom133_pl@protonmail.com>
2016-07-31 13:05:07 +0200

    Initial commit

---
+++ app/src/main/java/dom133/pl/updater/Main.java
@@ -0,0 +1,13 @@
+package dom133.pl.updater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
